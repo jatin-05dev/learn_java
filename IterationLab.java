70b74a1f02057ca4a341931193515fd8a56f714f
jatin-05dev <jatindev214@gmail.com>
2026-04-30 20:46:26 +0530

    code

--- IterationLab.java
+++ IterationLab.java
@@ -49,5 +49,13 @@ public class IterationLab {
             reversed += str.charAt(i);
         }
         return reversed;
+
+
+        public static String reverseString(String str) {
+        String reversed = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+        return reversed;
     }
 }
